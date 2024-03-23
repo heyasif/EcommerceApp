dab3111847aa184a0c6142d56aa64c640e8ee61e
Asif <heyasif@asif.local>
2024-03-23 13:11:35 +0530

    updated api

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const connnectDB = require('./Config/db');
 const ProductRouter = require('./Routes/Product.Router');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 app.use('/products', ProductRouter);
+app.use(cors());
 
 app.listen(process.env.PORT, () => {
   connnectDB();
