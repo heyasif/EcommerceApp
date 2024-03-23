6d0524c8759a52fc3237d7fabc868dc05427b471
Asif <heyasif@asif.local>
2024-03-23 13:31:32 +0530

    updated api 2

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,10 @@ require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 app.use('/products', ProductRouter);
-app.use(cors());
 
 app.listen(process.env.PORT, () => {
   connnectDB();
